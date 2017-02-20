b3507218a66c5ec6ea16c67e107f2eee41fbe990
zbw322092 <bz4e14@soton.ac.uk>
2017-02-20 10:53:53 +0800

    load modules as folder

--- Chapter_2/loadModules.js
+++ Chapter_2/loadModules.js
@@ -2,6 +2,7 @@ var myClass = require('./moduleExports');
 var module2 = require('./moduleExports2');
 // just delete the module cache, required modules can be used as usual.
 delete require.cache[require.resolve('./moduleExports2')]; 
+var groupModules = require('./modules_group');
 
 console.log(myClass.method());
 console.log(module2.method());
@@ -9,3 +10,8 @@ console.log(module2.method2());
 
 var fullyName = require.resolve('./moduleExports');
 console.log(fullyName);
+
+groupModules.one(); // module one
+groupModules.two(); // module two
+
+console.log(require.extensions); // { '.js': [Function], '.json': [Function], '.node': [Function] }
