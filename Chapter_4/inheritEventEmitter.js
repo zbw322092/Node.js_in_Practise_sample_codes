41e526d7e5931f8b5d1bd756166f2199d7529cc7
zbw322092 <bz4e14@soton.ac.uk>
2017-02-22 22:48:26 +0800

    inherit eventEmitter

---
+++ Chapter_4/inheritEventEmitter.js
@@ -0,0 +1,40 @@
+var util = require('util');
+var events = require('events');
+
+var AudioDevice = {
+	play: function(track) {
+		console.log('Track now playing:', track);
+	},
+	stop: function() {
+
+	}
+};
+
+function MusicPlayer() {
+	this.playing = false;
+	events.EventEmitter.call(this);
+}
+
+util.inherits(MusicPlayer, events.EventEmitter);
+
+var musicPlayer = new MusicPlayer();
+
+function play(track) {
+  this.playing = true;
+  AudioDevice.play(track);
+}
+
+musicPlayer.on('play', play);
+// musicPlayer.removeListener('play', play);
+
+musicPlayer.on('stop', function() {
+	this.playing = false;
+	AudioDevice.stop();
+});
+
+musicPlayer.emit('play', 'The Roots - The Fire');
+
+setTimeout(function() {
+	musicPlayer.emit('stop');
+}, 1000);
+
