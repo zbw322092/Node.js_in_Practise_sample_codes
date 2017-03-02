e91df742edfd5cc33f4697d09cab205c0e5378dc
zbw322092 <bz4e14@soton.ac.uk>
2017-03-02 22:51:53 +0800

    creating TCP clients to test servers

--- Chapter_7/aSimpleTCPServer.js
+++ Chapter_7/aSimpleTCPServer.js
@@ -1,5 +1,7 @@
+var assert = require('assert');
 var net = require('net');
 var clients = 0;
+var expectedAssertions = 2;
 
 var server = net.createServer(function(client) {
 	clients++;
@@ -10,10 +12,31 @@ var server = net.createServer(function(client) {
 		console.log('Client disconnected:', clientId);
 	});
 
-	client.write('Welcome client: ' + clientId + 'rn');
+	client.write('Welcome client: ' + clientId + '\r\n');
 	client.pipe(client);
 });
 
 server.listen(8000, function() {
 	console.log('Server is listening on port 8000');
+
+	runTest(1, function() {
+		runTest(2, function() {
+			console.log('Tests finished');
+			assert.equal(0, expectedAssertions);
+			server.close();
+		});
+	});
 });
+
+function runTest(expectedId, done) {
+	var client = net.connect(8000);
+
+	client.on('data', function(data) {
+		var expected = 'Welcome client: ' + expectedId + '\r\n';
+		assert.equal(data.toString(), expected);
+		expectedAssertions--;
+		client.end();
+	});
+
+	client.on('end', done);
+}
