594dc8c10e86dbeaa2908d33461ec758e4308965
zbw322092 <bz4e14@soton.ac.uk>
2017-03-06 23:42:11 +0800

    scripting web page with cherrio, process stream chunk ddata

--- Chapter_9/scriptingWebPageWithCheerio.js
+++ Chapter_9/scriptingWebPageWithCheerio.js
@@ -3,20 +3,28 @@ var https = require('https');
 var cheerio = require('cheerio');
 
 https.get('https://www.manning.com', function (res) {
-	// console.log(res);
+
 	var rawData;
 	res.on('data', function(chunk) {
-		rawData += chunk;
-	});
+		// rawData += chunk;
 
-	res.on('end', function() {
-		var $ = cheerio.load(rawData);
+		var $ = cheerio.load(chunk);
 		var releases = $('.book-cover-item-title');
 
 		releases.each(function(i) {
 			console.log('New Realease: ', $(this).text());
 		});
 
+	});
+
+	res.on('end', function() {
+		// var $ = cheerio.load(rawData);
+		// var releases = $('.book-cover-item-title');
+
+		// releases.each(function(i) {
+		// 	console.log('New Realease: ', $(this).text());
+		// });
+		console.log('done');
 
 	});
 });
